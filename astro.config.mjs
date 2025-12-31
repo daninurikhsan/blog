5ee04e226ab7ed2241d1c291b4e136a4485f396c
daninurikhsan <priadani25@gmail.com>
2025-12-31 21:54:16 +0800

    Update Route

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     site: "https://daninurikhsan.github.io",
-    base: "/blog/",
+    base: "/til/",
     output: "static",
     integrations: [tailwind()]
 });
